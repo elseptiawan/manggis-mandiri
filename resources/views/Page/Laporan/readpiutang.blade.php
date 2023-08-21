b305aaf82b6efaf800e9438156baed51a923863b
elseptiawan <septiawanelshandi@gmail.com>
2023-08-21 16:14:10 +0700

    develop halaman laporan

---
+++ resources/views/Page/Laporan/readpiutang.blade.php
@@ -0,0 +1,28 @@
+<div class="col-12">
+    <table class="table table-striped-columns">
+        <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nama Pelanggan</th>
+              <th scope="col">Detail</th>
+            </tr>
+          </thead>
+          
+          <tbody>
+            <input type="hidden" name="_token" id="csrf" value="{{Session::token()}}">
+            @php
+            $no = 1
+            @endphp
+            @foreach($data as $item)
+            <tr>
+                <th scope="row">{{ $no++ }}</th>
+                <td>{{ $item->pelanggan->nama_pelanggan }}</td>
+                <td><i class="bi bi-card-list" style="margin-right: 5px; color: blue; cursor: pointer" onClick="event.preventDefault();detailpiutang({{ $item->pelanggan_id }})"></i></td>
+                  {{-- <td><i class="bi bi-pencil-square" style="margin-right: 5px; color: green; cursor: pointer" onClick="event.preventDefault();edit({{ $item->id }})"></i>
+                    <i class="bi bi-x-square-fill" style="color: #f43737; cursor: pointer" onClick="event.preventDefault();destroy({{ $item->id }})"></i>
+                </td> --}}
+            </tr>
+            @endforeach
+          </tbody>
+    </table>
+</div>
