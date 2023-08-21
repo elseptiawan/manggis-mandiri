e6f2005185e2ffc0b1eecf838fd2672c3d2e8314
elseptiawan <septiawanelshandi@gmail.com>
2023-08-21 16:12:53 +0700

    redirecting middleware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,13 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if ($request->user()->role == "pelanggan"){
+                    return redirect()
+                        ->to(route('piutang'));
+                }
+                else{
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
 
